eda8be47cfe36a6f2d8d40df3adf8d81f9914a3a
Zach Dodson <zdodson21@gmail.com>
2024-09-30 18:08:17 -0400

    config modifications

--- eslint.config.js
+++ eslint.config.js
@@ -5,4 +5,10 @@ import pluginJs from "@eslint/js";
 export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
+  {
+    "extends": [
+      "some-other-config-you-use",
+      "prettier"
+    ]
+  }
 ];
