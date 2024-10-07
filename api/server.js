4772adcac568f39d816aaf9674bee2e244424840
Zach Dodson <zdodson21@gmail.com>
2024-10-07 11:06:07 -0400

    small changes, should not break anything

--- api/server.js
+++ api/server.js
@@ -5,16 +5,32 @@ export default async function handler(request, response) {
   try {
     // Get the information to be written to DB
     const bookName = request.query.bookName;
+    const bookAuthor = request.query.bookAuthor;
+    const bookPrice = request.query.bookPrice;
     const bookGenre = request.query.bookGenre;
+    const customerName = request.query.customerName;
+    const customerEmail = request.query.customerEmail;
+    const customerPhone = request.query.customerPhone;
 
     // Check that we have the required information
-    if (!bookName) throw new Error("Missing customer name");
-    if (!bookGenre) throw new Error("Missing customer age");
+    if (!bookName) throw new Error("Missing book name");
+    if (!bookAuthor) throw new Error("Missing book author");
+    if (!bookPrice) throw new Error("Missing book price");
+    if (!bookGenre) throw new Error("Missing book genre");
+    if (!customerName) throw new Error("Missing customer name");
+    if (!customerEmail) throw new Error("Missing customer email");
+    if (!customerPhone) throw new Error("Missing customer phone");
+
     // Write to DB
     await sql`
       INSERT INTO Books (name, genre) VALUES (
         ${bookName}, 
-        ${bookGenre}
+        ${bookAuthor},
+        ${bookPrice},
+        ${bookGenre},
+        ${customerName},
+        ${customerEmail},
+        ${customerPhone}
       );
     `;
   } catch(error) {
@@ -24,7 +40,7 @@ export default async function handler(request, response) {
   const books = await sql`
     SELECT * FROM Books;
   `;
-  return response.status(200).json({ books});
+  return response.status(200).json({ books });
 }
 
 // Test URL to add to DB
