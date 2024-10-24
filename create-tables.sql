4abfe573c7274dbd485661522f3138c965f8a6f0
Zach Dodson <zdodson21@gmail.com>
2024-10-24 10:31:01 -0400

    created SQL file

---
+++ create-tables.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Data (
+  BookName varchar(255),
+  BookAuthor varchar(255),
+  BookPrice Decimal(10,2),
+  BookGenre varchar(255),
+  CustomerName varchar(255),
+  CustomerEmail varchar(255),
+  CustomerPhone varchar(255)
+);
